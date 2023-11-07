5dab493fdee22cc03490a6b15b0178416a9c2449
Jululuz <134288462+julia-luz@users.noreply.github.com>
2023-11-07 10:28:49 -0300

    fixed overwrite and loops

--- map-ont.py
+++ map-ont.py
@@ -33,6 +33,7 @@ import pysam
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+os.environ['XDG_SESSION_TYPE'] = 'x11'
 
 from eccFinder_lib.utilities import log,run_oae,get_eccFinder_version
 from eccFinder_lib.Aligner import Minimap2SAMAligner
@@ -40,10 +41,43 @@ from eccFinder_lib.Aligner import Minimap2Aligner
 from eccFinder_lib.Spliter import tidehunter
 from eccFinder_lib.Peaker import genrich
 
-def read_genome_alignments(file_prefix,align_path,min_qlen,min_aln, overwrite_files):
+def read_genome_alignments(file_prefix,align_path,min_qlen,min_aln,overwrite_files):
     """ Filtering the raw read alignments based on query length and alignment length."""
     if os.path.isfile(align_path + file_prefix+".paf.bed"):
-        log("INFO", "Retaining pre-existing file: " + align_path +file_prefix+".paf.bed")
+        if not overwrite_files:
+            log("INFO", "Retaining pre-existing file: " + align_path +file_prefix+".paf.bed")
+        else:
+            log("INFO", "Overwriting pre-existing file: " + align_path +file_prefix+".paf.bed")
+            with open(align_path + file_prefix+".paf") as f:   
+                outfile=open(align_path + file_prefix+".paf.bed",'w')
+                headers = ['refID','rstart', 'rend','queryID','qlen','direction']
+                total=0
+                n_primary=0
+                for line in f: 
+                    parts = line.strip().split("\t")
+                    total=total+1
+                    resultss = {
+		            "queryID": parts[0],
+		            "qlen":  int(parts[1]),
+		            "qstart": int(parts[2]),
+		            "qend": int(parts[3]),
+		            "direction": parts[4],
+		            "refID": parts[5],
+		            "rlen": int(parts[6]),
+		            "rstart": int(parts[7]),
+		            "rend": int(parts[8]),
+                    "rdis": int(parts[8])-int(parts[7]),
+		            "allmatch": int(parts[9]),
+                    "blockmatch": int(parts[10]),
+                    }
+                    if min_aln is not None and resultss['rdis'] < min_aln:
+                        n_primary=n_primary+1
+                        continue
+                    if min_qlen is not None and resultss['qlen'] < min_qlen:
+                        continue
+                    out_row = (str(resultss[x]) for x in headers)
+                    outfile.write('\t'.join(out_row))
+                    outfile.write('\n')
     else:
         with open(align_path + file_prefix+".paf") as f:   
             outfile=open(align_path + file_prefix+".paf.bed",'w')
@@ -93,13 +127,13 @@ def run_TideHunter(file_prefix,query_file,peak_path, num_threads, max_divergence
         TH_cmd = "TideHunter"+ TH_params+ str(query_file)+ " -u > "+ peak_path +file_prefix+".unit.fa"
         subprocess.call(TH_cmd, shell=True) 
 
-def run_samtools(file_prefix,output_path,peak_path,num_threads, overwrite_files):
+def run_samtools(file_prefix,output_path,peak_path,num_threads,overwrite_files):
     """ sort, filter and index alignments. """
     if os.path.isfile(peak_path +file_prefix+".unit.bam"):
         if not overwrite_files:
             log("INFO", "Retaining pre-existing file: " + peak_path +file_prefix+".unit.bam")
         else:
-            log("INFO", "Retaining pre-existing file: " + peak_path +file_prefix+".unit.bam")
+            log("INFO", "Overwriting pre-existing file: " + peak_path +file_prefix+".unit.bam")
             pysam.sort("-@", str(num_threads),"-n",  "-o", output_path+"tmp1", output_path +file_prefix+".tmp.sam", catch_stdout=False)
             pysam.view("-@", str(num_threads),"-h", "-o", output_path+"tmp2",output_path +"tmp1", catch_stdout=False)
             cmd = "awk -v OFS='\\t' '{if ($1 !~ /^@/ )$1=$1\"/2\"}1' " + output_path+"tmp2" + ">"+ peak_path +file_prefix+".unit.bam"
@@ -110,14 +144,13 @@ def run_samtools(file_prefix,output_path,peak_path,num_threads, overwrite_files)
         cmd = "awk -v OFS='\\t' '{if ($1 !~ /^@/ )$1=$1\"/2\"}1' " + output_path+"tmp2" + ">"+ peak_path +file_prefix+".unit.bam"
         subprocess.call(cmd, shell=True)
 
-def run_Genrich(file_prefix,output_path,peak_path,num_threads, min_peak,max_dist,max_pvalue,overwrite_files):
+def run_Genrich(file_prefix,output_path,peak_path,min_peak,max_dist,max_pvalue,overwrite_files):
     """ Detecting sites of genomic enrichment. """
     if os.path.isfile(output_path +file_prefix+".site.bed"):
         if not overwrite_files:
             log("INFO", "Retaining pre-existing file: " + output_path +file_prefix+".site.bed")
         else:
             log("INFO", "Overwriting pre-existing file: " + output_path +file_prefix+".site.bed")
-            run_samtools(file_prefix,output_path, num_threads, overwrite_files)
             GR_params = " -yv "
             GR_params += " -l " + str(min_peak)+" -g " + str(max_dist) +" -p " + str(max_pvalue) 
             GR_cmd = "Genrich -t "+ peak_path +file_prefix+".unit.bam" + GR_params+ " -o "+ peak_path +file_prefix+".site"
@@ -134,8 +167,64 @@ def run_Genrich(file_prefix,output_path,peak_path,num_threads, min_peak,max_dist
 
 def run_filterBED(file_prefix,output_path, align_path,min_read,min_bound,min_cov,overwrite_files):
     if os.path.isfile(output_path +file_prefix +".csv"):
-        log("INFO", "Filtering locus by repeat units in one read")
-    else:  
+        if not overwrite_files: 
+            log ("INFO", "Retaining pre-existing file: " + output_path +file_prefix +".csv")
+        else:
+            log ("INFO", "Overwriting pre-existing file: " + output_path +file_prefix +".csv")
+            bedtools_params= " -wao -f "+ str(min_bound) 
+            tmp1=output_path +file_prefix+".paf.bed.tmp1"
+            cmd ="bedtools intersect -a "+ output_path +file_prefix+".site.bed -b " + align_path +file_prefix + ".paf.bed" + bedtools_params+ " -nonamecheck > "+tmp1
+            subprocess.call(cmd, shell=True) 
+
+            cmd1 = "cat "+ output_path +file_prefix+".paf.bed.tmp1"
+            cmd2 = "awk '$10>0'| sort -k7,7 -k9,9 | groupBy -g 1,2,3,7,9 -c 7 -o count |awk '$6>1'"
+            cmd3 = "bedtools sort |groupBy -g 1,2,3 -c 4,6 -o count_distinct,sum -nonamecheck|awk '$4>2' > " +output_path +file_prefix+".paf.bed.tmp2"
+            sub = "{inS} |{group}|{bed} ".format(inS=cmd1, group=cmd2, bed=cmd3)
+            ps = subprocess.Popen(sub,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+            output = ps.communicate()[0]
+
+            log("INFO", "Filtering locus by boundary coverage. ")
+            csv=output_path +file_prefix +".tmp.csv"
+            cmd ="bedtools coverage -counts -a "+ output_path +file_prefix+".paf.bed.tmp2 -b " + align_path +file_prefix + ".paf.bed -f " + str(min_bound)+ " -nonamecheck > "+csv
+            subprocess.call(cmd, shell=True) 
+
+            # Write the ecc_finder.csv.
+            log("INFO", "Filtering locus by mininum number of tandely repeated long reads ")
+            with open(output_path + file_prefix+".tmp.csv") as f:   
+                outfile=open(output_path + file_prefix+".csv",'w')
+                headers = ['refID','rstart','rend','read_number','repeat_unit','coverage','ecc_len']
+                total=0
+                n_primary=0
+                for line in f:
+                    parts = line.strip().split("\t")
+                    total=total+1
+                    resultss = {
+	                "refID": parts[0],
+	                "rstart":  int(parts[1]),
+	                "rend": int(parts[2]),
+	                "read_number": int(parts[3]),
+	                "repeat_unit": parts[4],
+                    "coverage": int(parts[4]),
+                    "ecc_len":int(parts[2])-int(parts[1]),
+                    }
+                    if min_read is not None and resultss['read_number'] < min_read:
+                        n_primary=n_primary+1
+                        continue
+                    if min_cov is not None and resultss['coverage'] < min_cov:
+                        continue
+                    out_row = (str(resultss[x]) for x in headers)
+                    outfile.write('\t'.join(out_row))
+                    outfile.write('\n')
+
+            for filename in glob.glob(os.path.join(output_path, "*tmp*")):
+                try:
+                    #Trying to remove a current file
+                    os.remove(os.path.join(output_path, filename))
+                except EnvironmentError:
+                    #You don't have permission to do it
+                    pass
+    else:
+        log("INFO", "Filtering locus by repeat units in one read") 
         bedtools_params= " -wao -f "+ str(min_bound) 
         tmp1=output_path +file_prefix+".paf.bed.tmp1"
         cmd ="bedtools intersect -a "+ output_path +file_prefix+".site.bed -b " + align_path +file_prefix + ".paf.bed" + bedtools_params+ " -nonamecheck > "+tmp1
@@ -161,6 +250,7 @@ def run_filterBED(file_prefix,output_path, align_path,min_read,min_bound,min_cov
             total=0
             n_primary=0
             for line in f: 
+                log ("INFO", line)
                 parts = line.strip().split("\t")
                 total=total+1
                 resultss = {
@@ -199,7 +289,7 @@ def run_getFasta(output_path, file_prefix ,ref_genome,overwrite_files):
             subprocess.call(cmd, shell=True) 
     else: 
         cmd ="seqtk subseq "+ ref_genome+ " " + output_path +file_prefix +".csv" + "> "+output_path +file_prefix +".fasta"
-        subprocess.call(cmd, shell=True) 
+        subprocess.call(cmd, shell=True)
 
 def main():
     description = "A tool to detect eccDNA loci using ONT sequencing"
@@ -209,8 +299,7 @@ def main():
     parser.add_argument("-r", metavar="<query.fasta>", default="", type=str, help="reference genome fasta file (uncompressed or bgzipped)")
 
     map_options = parser.add_argument_group("map options")   
-    map_options.add_argument('-t', metavar="INT",type=int, default=get_default_thread(),
-                            help='number of CPU threads for mapping mode')
+    map_options.add_argument('-t', metavar="INT",type=int, default=get_default_thread(), help='number of CPU threads for mapping mode')
     mm2_default = "-x map-ont"
 
     map_options.add_argument("-g", metavar="STR", type=str, default="", help="reference genome size larger than 4Gb [yes]")
@@ -225,7 +314,7 @@ def main():
     peak_options.add_argument("-p", metavar="FLT", type=float, default=0.05, help="maximum p-value [0.05]")
 
     val_options = parser.add_argument_group("validation options")
-    val_options.add_argument("-n", metavar="INT", type=int, default=2, help="minimum copy number of tandem repeat in a long read [2]")
+    val_options.add_argument("-n", metavar="INT", type=int, default=2, help="minimum copy number of tandmin_boundem repeat in a long read [2]")
     val_options.add_argument("-e", metavar="FLT", type=float, default=0.25, help="maximum allowed divergence rate between two consecutive repeats [0.25]")
     val_options.add_argument("-s", metavar="INT", type=int, default=30, help="minimum period size of tandem repeat (>=2) [30]")
     val_options.add_argument("--min-read", metavar="INT", type=int, default=3, help="filter locus by unique mapped read number [3]")
@@ -336,8 +425,8 @@ def main():
     map_all = Minimap2SAMAligner(idx_file, [peak_path +file_prefix+".unit.fa"],mapont_aligner_path, mm2_params,pre , in_overwrite=overwrite_files)
     map_all.run_aligner()
 
-    run_samtools(file_prefix,output_path,peak_path,num_threads, overwrite_files)
-    run_Genrich(file_prefix,output_path,peak_path,num_threads, min_peak,max_dist,max_pvalue,overwrite_files)
+    run_samtools(file_prefix,output_path,peak_path,num_threads,overwrite_files)
+    run_Genrich(file_prefix,output_path,peak_path,min_peak,max_dist,max_pvalue,overwrite_files)
 
     #Peak calling
     log("INFO", "Producing bed file of eccDNA locus")
